158d66cda548a5f4cf4363a9b325e2de56332dc4
Jess Green <ardath.ksheyna78@gmail.com>
2018-02-01 20:04:02 -0600

    Adding providers for Twig bridge

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        TwigBridge\ServiceProvider::class,
     ],
 
     /*
@@ -222,6 +222,7 @@ return [
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
+        'Twig' => TwigBridge\Facade\Twig::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
