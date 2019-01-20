b43767d4cf6cbabed6c5d089cf523757e32fff22
jesgs <ardath.ksheyna78@gmail.com>
2019-01-20 17:30:36 -0600

    Removing missing trait

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,7 +3,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
-use TCG\Voyager\Traits\HasRelationships;
 use TCG\Voyager\Traits\Translatable;
 
 
